bc833aa88583b4de0c059f549fd6b82b340c368e
KarolisGaiv <kgaivenis@gmail.com>
2021-04-08 21:37:49 +0300

    Init commit

---
+++ script.js
@@ -0,0 +1,73 @@
+let myLibrary = [
+    {
+        name: "Hobbit",
+        author: "Tolken",
+        pages: 295,
+        read: false
+    },
+    {
+        name: "Angels and Demons",
+        author: "Dan Brown",
+        pages: 300,
+        read: true
+    }
+];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+
+function addBookToLibrary() {
+    const book = new Book(bookName.value, bookAuthor.value, bookPages.value, bookStatus.value)
+
+    if (bookName == "" || bookAuthor == "") {
+        errMsg.innerHTML = "Please enter something"
+        return false;
+    }
+
+    myLibrary.push(book)
+    document.querySelector(".form").reset()
+    resetFormFields()
+}
+
+function resetFormFields() {
+    bookName = ""
+    bookAuthor = ""
+    bookPages = ""
+    bookStatus = ""
+}
+
+let bookName = document.getElementById("book-name")
+let bookAuthor = document.getElementById("book-author")
+let bookPages = document.getElementById("book-pages")
+let bookStatus = document.getElementById("book-status")
+const addBtn = document.querySelector(".add-btn")
+const errMsg = document.querySelector(".err-msg")
+
+
+
+addBtn.addEventListener("click", addBookToLibrary)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const cardName = document.querySelector(".book-name")
+const cardAuthor = document.querySelector(".book-author")
+const cardPages = document.querySelector(".book-pages")
+const cardStatus = document.querySelector(".book-status")
